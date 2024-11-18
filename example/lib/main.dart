398c42b03489ce8eda3a9f286d72e4fe993791d0
Pranim Singh Thakuri <pranimsingh7@gmail.com>
2024-11-18 11:32:48 +0545

    [#1] Example created for the package

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Animated TextField',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
@@ -28,22 +28,51 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Center(
-            child: AnimatedTextFormField(
-          borderColor: Colors.amber,
-          decoration: const InputDecoration(
-            hintText: "sddasd",
-            labelText: 'ad',
-            border: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.grey),
-                borderRadius: BorderRadius.all(Radius.circular(50.0))),
-            // focusedBorder: OutlineInputBorder(
-            //   borderSide: BorderSide(),
-            // ),
-          ),
-        )),
+      appBar: AppBar(
+        title: const Text('Animated TextFormField Example'),
+        backgroundColor: const Color.fromARGB(255, 101, 71, 152),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //
+            // First AnimatedTextFormField
+            //
+            AnimatedTextFormField(
+              textInputAction: TextInputAction.next,
+              decoration: InputDecoration(
+                hintText: "Enter your name",
+                labelText: 'Name',
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey),
+                  borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.deepPurple, width: 2.0),
+                ),
+              ),
+            ),
+            //
+            SizedBox(height: 20), // Space between the text fields
+            //
+            // Second AnimatedTextFormField
+            AnimatedTextFormField(
+              decoration: InputDecoration(
+                hintText: "Enter your email",
+                labelText: 'Email',
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey),
+                  borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.deepPurple, width: 2.0),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
